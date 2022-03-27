42c065f969ae29b15bfa87f137c995261dad98b4
junioranheu <junioranheu@gmail.com>
2022-03-27 14:10:12 -0300

    criar estrutura inicial

--- components/outros/sidebar.js
+++ components/outros/sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Styles from '../../styles/sidebar.module.css';
+import Biblioteca from '../svg/biblioteca';
+import Casa from '../svg/casa';
+import Lupa from '../svg/lupa';
+import SpotifyLogo from '../svg/spotifyLogo';
 
 export default function Sidebar() {
     return (
         <aside className={Styles.sidebar}>
-            aea
+            <SpotifyLogo width='130px' />
+
+            <span>
+                <Casa width='24px' /> <span>Início</span>
+            </span>
+
+            <span>
+                <Lupa width='24px' /> <span>Procurar</span>
+            </span>
+
+            <span>
+                <Biblioteca width='24px' /> <span>Biblioteca</span>
+            </span>
         </aside>
     )
 }

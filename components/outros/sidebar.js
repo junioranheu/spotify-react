085186e8e972ebd4ad4d97f91d4e877dfbdee154
junioranheu <junioranheu@gmail.com>
2022-03-28 11:19:11 -0300

    ajustes gerais

--- components/outros/sidebar.js
+++ components/outros/sidebar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Styles from '../../styles/sidebar.module.css';
 import Biblioteca from '../svg/biblioteca';
 import Casa from '../svg/casa';
+import Coracao from '../svg/coracao';
 import Lupa from '../svg/lupa';
+import Mais from '../svg/mais';
 import SpotifyLogo from '../svg/spotifyLogo';
 
 export default function Sidebar() {
@@ -27,8 +29,21 @@ export default function Sidebar() {
             </div>
 
             <div className={Styles.divIcones}>
-                <span>Criar lista de reprodução</span>
-                <span>Músicas que gostou</span>
+                <span>
+                    <span className={`${Styles.quadrado} ${Styles.quadradoBranco}`}>
+                        <Mais width='12px' cor='var(--preto)' />
+                    </span>
+
+                    <span>Criar lista de reprodução</span>
+                </span>
+
+                <span>
+                    <span className={`${Styles.quadrado} ${Styles.quadradoColorido}`}>
+                        <Coracao width='12px' cor='var(--branco)' />
+                    </span>
+
+                    <span>Músicas curtidas</span>
+                </span>
             </div>
         </aside>
     )

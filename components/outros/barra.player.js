79445d140f438ea45ef85cf5521b968081f172ff
junioranheu <junioranheu@gmail.com>
2022-03-29 20:17:38 -0300

    ajustes em barra.player.js e suas dependências

--- components/outros/barra.player.js
+++ components/outros/barra.player.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import ImgTheStroke from '../../static/thestrokes.webp';
 import Styles from '../../styles/barra.player.module.css';
 import Coracao from '../svg/barra.player/coracao';
+import CoracaoPreenchido from '../svg/barra.player/coracaoPreenchido';
 import Dispositivo from '../svg/barra.player/dispositivo';
 import Fullscreen from '../svg/barra.player/fullscreen';
 import Lista from '../svg/barra.player/lista';
@@ -9,16 +12,29 @@ import Toggle from '../svg/barra.player/toggle';
 import Volume from '../svg/barra.player/volume';
 
 export default function BarraPlayer() {
+    const [isMusicaCurtida, setIsMusicaCurtida] = useState(false);
+
     return (
         <section className={Styles.barraPlayer}>
             <div className={Styles.divInfo}>
-                <Coracao />
+                <div>
+                    <div>
+                        <Image src={ImgTheStroke} width={56} height={56} />
+                    </div>
+
+                    <div className={Styles.infoMusica}>
+                        <span className={Styles.infoTitulo}>bla bla bla</span>
+                        <span className={Styles.infoDescricao}>vai tomar no cu</span>
+                    </div>
+                </div>
+ 
+                {isMusicaCurtida ? (
+                    <CoracaoPreenchido />
+                ) : (
+                    <Coracao />
+                )}
+
                 <Toggle />
-                <Microfone />
-                <Lista />
-                <Dispositivo />
-                <Volume />
-                <Fullscreen />
             </div>
 
             <div className={Styles.divPlayer}>
@@ -26,7 +42,12 @@ export default function BarraPlayer() {
             </div>
 
             <div className={Styles.divOpcoes}>
-                <h1>Parte 3</h1>
+                <Microfone />
+                <Lista />
+                <Dispositivo />
+                <Volume />
+                <span>volume</span>
+                <Fullscreen />
             </div>
         </section>
     )

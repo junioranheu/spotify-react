5ea5f06a98569c34a818a94124a38198358ef451
junioranheu <junioranheu@gmail.com>
2022-04-01 22:31:57 -0300

    getStaticProps API

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import { loremIpsum } from 'lorem-ipsum';
 import React, { useEffect } from 'react';
 import Styles from '../styles/index.module.css';
 
-export default function Index() {
+export default function Index({ musicas }) {
     useEffect(() => {
         // Título da página;
         document.title = 'Spotify — React.js — junioranheu';
@@ -10,42 +9,24 @@ export default function Index() {
 
     return (
         <section className={Styles.container}>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
-            <h1>{loremIpsum({ count: 1 })}</h1>
+            <ul>
+                {musicas.map((m) => (
+                    <li key={m.musicaId}>
+                        {m.nome} - {m.musicasBandas[0].bandas.nome}
+                    </li>
+                ))}
+            </ul>
         </section>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch('https://spotifyapi.azurewebsites.net/api/Musicas/todos')
+    const musicas = await res.json();
+
+    return {
+        props: {
+            musicas,
+        },
+    }
 }

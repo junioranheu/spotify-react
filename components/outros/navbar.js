444c649aaf9e24da28e9ff6fd24c3e54fa417eba
junioranheu <junioranheu@gmail.com>
2022-04-05 12:24:31 -0300

    corrigir bug em navbar.js

--- components/outros/navbar.js
+++ components/outros/navbar.js
@@ -1,12 +1,20 @@
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Botao from '../../components/outros/botao.js';
 import Styles from '../../styles/navbar.module.css';
 import { Auth, UsuarioContext } from '../../utils/context/usuarioContext';
 
 export default function Navbar() {
     const [isAuth, setIsAuth] = useContext(UsuarioContext); // Contexto do usuário;
+    const [nomeUsuario, setNomeUsuario] = useState('');
+
+    useEffect(() => {
+        console.log(isAuth);
+        console.log(Auth.getUsuarioLogado()?.nome);
+
+        setNomeUsuario(isAuth ? Auth.getUsuarioLogado()?.nome : null);
+    }, [isAuth]);
 
     function deslogar() {
         NProgress.start();
@@ -24,11 +32,13 @@ export default function Navbar() {
 
     return (
         <nav className={Styles.navbar}>
-            {/* <h1>aea</h1> */}
+            {/* <div>
+                <h1>aea</h1>
+            </div> */}
 
             <div className={Styles.divDireita}>
                 {isAuth ? (
-                    <h1 onClick={() => deslogar()}>LOGADO</h1>
+                    <h1 onClick={() => deslogar()}>{nomeUsuario}</h1>
                 ) : (
                     <Botao texto={'Entrar'} url={'/entrar'} isNovaAba={false} Svg='' />
                 )}
